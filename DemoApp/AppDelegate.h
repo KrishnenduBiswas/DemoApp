53914096b94a8e4cb775e510bd8599550217495f
os4ed <os4ed@os4eds-mac-mini.local>
2016-06-03 19:04:46 +0530

    Initial Commit

---
+++ DemoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoApp
+//
+//  Created by os4ed on 6/3/16.
+//  Copyright © 2016 os4ed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoApp
+//
+//  Created by os4ed on 6/3/16.
+//  Copyright © 2016 os4ed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
